5398f7aa2bfc070f62a895ef79fbfc978786a044
Maxim <maximgran@gmail.com>
2019-04-03 23:03:39 +0300

    Check
    Pushed from GitKraken

--- src/main/java/com/example/first/Comment/CommentRepo.java
+++ src/main/java/com/example/first/Comment/CommentRepo.java
@@ -5,9 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.ArrayList;
 
 public interface CommentRepo extends JpaRepository<CommentRecord, Integer> {
-
-    CommentRecord getById(int id);
-
-
     ArrayList<CommentRecord> getAllByRequestId(int requestId);
 }
